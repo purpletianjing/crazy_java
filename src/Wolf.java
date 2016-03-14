ae111ce0496ffbf8d2ebc3fc78a83e3032155894
purpletianjing <TianjingSun@outlook.com>
2016-03-14 17:27:46 +0800

    add composite

--- src/Wolf.java
+++ src/Wolf.java
@@ -6,13 +6,13 @@ class Creature {
 
 class Animal extends Creature {
     public Animal(String name) {
-        System.out.println("Animal 带一个参数的构造器, "
+        System.out.println("Composite.Animal 带一个参数的构造器, "
                 + "该动物的name为" + name);
     }
 
     public Animal(String name, int age) {
         this(name);
-        System.out.println("Animal 带两个参数的构造器, "
+        System.out.println("Composite.Animal 带两个参数的构造器, "
                 + "其age为" + age);
     }
 }
