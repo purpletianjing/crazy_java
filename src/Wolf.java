5850a6af42b57d10b70301e633e30a7e70c5c5a9
purpletianjing <TianjingSun@outlook.com>
2016-03-14 09:32:20 +0800

    test constructor when class extends

---
+++ src/Wolf.java
@@ -0,0 +1,28 @@
+class Creature {
+    public Creature() {
+        System.out.println("Creature 无参数构造器");
+    }
+}
+
+class Animal extends Creature {
+    public Animal(String name) {
+        System.out.println("Animal 带一个参数的构造器, "
+                + "该动物的name为" + name);
+    }
+
+    public Animal(String name, int age) {
+        this(name);
+        System.out.println("Animal 带两个参数的构造器, "
+                + "其age为" + age);
+    }
+}
+
+public class Wolf extends Animal {
+    public Wolf() {
+        super("灰太狼", 3);
+        System.out.println("Wolf无参数的构造器");
+    }
+    public static void main(String[] args) {
+        new Wolf();
+    }
+}
