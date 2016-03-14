8fa511a74e6bceb55dbfa07abb5b2ba44bfdf333
purpletianjing <TianjingSun@outlook.com>
2016-03-14 23:10:33 +0800

    test static initial part

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,47 @@
-/**
- * Created by twer on 3/14/16.
- */
+class Root {
+    static {
+        System.out.println("Root的静态初始化");
+    }
+    {
+        System.out.println("Root的普通初始化");
+    }
+    public Root() {
+        System.out.println("Root的无参数构造器");
+    }
+}
+
+class Mid extends Root {
+    static {
+        System.out.println("Mid 的静态初始化");
+    }
+    {
+        System.out.println("Mid 的普通初始化");
+    }
+    public Mid() {
+        System.out.println("Mid的无参数构造器");
+    }
+    public Mid(String msg) {
+        this();
+        System.out.println("Mid 的带参数构造器,其参数为:" + msg);
+    }
+}
+
+class Leaf extends Mid {
+    static {
+        System.out.println("Leaf的静态初始化");
+    }
+    {
+        System.out.println("Leaf的普通初始化");
+    }
+    public Leaf() {
+        super("疯狂Java讲义");
+        System.out.println("执行Leaf的构造器");
+    }
+}
+
 public class Test {
+    public static void main(String[] args) {
+        new Leaf();
+        new Leaf();
+    }
 }
