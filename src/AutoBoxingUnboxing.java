e8d9b4740a55822abce146270f3d503b81861ee6
purpletianjing <TianjingSun@outlook.com>
2016-03-16 10:29:43 +0800

    transfer bool to Boolean

---
+++ src/AutoBoxingUnboxing.java
@@ -0,0 +1,18 @@
+public class AutoBoxingUnboxing {
+    public static void main(String[] args) {
+        //直接把一个基本类型变量赋给Integer对象
+        Integer inObj = 5;
+
+        //直接把一个boolean类型变量赋给一个object类型变量
+        Object boolObj = true;
+
+        //直接把一个Integer对象赋给int类型变量
+        int it = inObj;
+        if (boolObj instanceof Boolean) {
+            //先把Object对象强制类型转换为Boolean类型,再赋给boolean变量
+            boolean b = (Boolean) boolObj;
+            System.out.println(b);
+            System.out.println(it);
+        }
+    }
+}
